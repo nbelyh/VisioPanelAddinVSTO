95d2bfd1b271ca5826d19272ec1431fff1ebdeb9
Nikolay Belykh <nbelyh@gmail.com>
2020-05-31 01:24:56 +0200

    Fix vp project visibility, remove explicit 4.0 mentioning, fix copyright, add tags

--- TemplateCS/Properties/AssemblyInfo.cs
+++ TemplateCS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TemplateCS.Properties")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2010-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- TemplateVB/Properties/AssemblyInfo.vb
+++ TemplateVB/Properties/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("")>
-<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyCopyright("Copyright ©  2010-2020")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
